a063fa0021802ac5da3c62ab84d215129481fbcc
ELagunova <elvlagunova@ya.ru>
2021-06-25 12:14:59 +0300

    Выделен вспомогательный класс Contact

---
+++ contact.py
@@ -0,0 +1,12 @@
+class Contact:
+
+    def __init__(self, first_name, last_name, address, mobile, email, bday, bmonth, byear, notes):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+        self.mobile = mobile
+        self.email = email
+        self.bday = bday
+        self.bmonth = bmonth
+        self.byear = byear
+        self.notes = notes
--- test_add_contact.py
+++ test_add_contact.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
+from contact import Contact
 import unittest
 
 
@@ -21,7 +22,10 @@ class TestAddContact(unittest.TestCase):
         wd = self.wd
         self.open_login_page(wd)
         self.login(wd, username="admin", password="secret")
-        self.add_new_contact(wd)
+        self.add_new_contact(wd, Contact(first_name="Tom", last_name="Walles",
+                             address="701 First Ave, Houston, TX 77007", mobile="+71243212555",
+                             email="tomwalles77@gmai.com", bday="10", bmonth="January", byear="1992",
+                             notes="Tom, Houston"))
         self.open_home_page(wd)
         self.logout(wd)
 
@@ -31,23 +35,23 @@ class TestAddContact(unittest.TestCase):
     def open_home_page(self, wd):
         wd.find_element_by_link_text("home page").click()
 
-    def add_new_contact(self, wd):
+    def add_new_contact(self, wd, contact):
         wd.find_element_by_link_text("add new").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("Tom")
+        wd.find_element_by_name("firstname").send_keys(contact.first_name)
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("Walles")
+        wd.find_element_by_name("lastname").send_keys(contact.last_name)
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("701 First Ave, Houston, TX 77007")
+        wd.find_element_by_name("address").send_keys(contact.address)
         wd.find_element_by_name("mobile").clear()
-        wd.find_element_by_name("mobile").send_keys("+71243212555")
+        wd.find_element_by_name("mobile").send_keys(contact.mobile)
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("tomwalles77@gmai.com")
-        Select(wd.find_element_by_name("bday")).select_by_visible_text("10")
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text("January")
-        wd.find_element_by_name("byear").send_keys("1992")
+        wd.find_element_by_name("email").send_keys(contact.email)
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.bday)
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.bmonth)
+        wd.find_element_by_name("byear").send_keys(contact.byear)
         wd.find_element_by_name("notes").clear()
-        wd.find_element_by_name("notes").send_keys("Tom, Houston")
+        wd.find_element_by_name("notes").send_keys(contact.notes)
         wd.find_element_by_name("submit").click()
 
     def login(self, wd, username, password):
