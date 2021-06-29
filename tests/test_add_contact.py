1e5c3cd0257f8925688d0718e77de226557524c3
ELagunova <elvlagunova@ya.ru>
2021-06-29 23:10:00 +0300

    Выделен класс для управления сессиями

--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -12,18 +12,18 @@ def app(request):
 
 
 def test_add_contact(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.add_new_contact(Contact(first_name="Tom", last_name="Walles",
                         address="701 First Ave, Houston, TX 77007", mobile="+71243212555",
                         email="tomwalles77@gmai.com", bday="10", bmonth="January", byear="1992",
                         notes="Tom, Houston"))
-    app.logout()
+    app.session.logout()
 
 
 def test_add_contact_without_email(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     app.add_new_contact(Contact(first_name="Richard", last_name="Baket",
                         address="123 Second Ave, Houston, TX 77008", mobile="+7127431239",
                         email="", bday="17", bmonth="June", byear="1996",
                         notes="Richard, Houston"))
-    app.logout()
+    app.session.logout()
