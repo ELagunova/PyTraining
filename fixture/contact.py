e94f46f24603f29da52b08f708a508902c66b0ba
ELagunova <elvlagunova@ya.ru>
2021-07-01 12:52:38 +0300

    Добавлены тесты удаления групп и контактов

--- fixture/contact.py
+++ fixture/contact.py
@@ -27,6 +27,22 @@ class ContactHelper:
         wd.find_element_by_name("submit").click()
         self.open_home_page()
 
+    def delete_first_contact(self):
+        wd = self.app.wd
+        #select first contact
+        wd.find_element_by_name("selected[]").click()
+        #submit deletion
+        wd.find_element_by_xpath("//input[@value='Delete']").click()
+        wd.switch_to.alert.accept()
+
+    def delete_all_contacts(self):
+        wd = self.app.wd
+        #select all contacts
+        wd.find_element_by_id("MassCB").click()
+        #submit deletion
+        wd.find_element_by_xpath("//input[@value='Delete']").click()
+        wd.switch_to.alert.accept()
+
     def open_home_page(self):
         wd = self.app.wd
         wd.find_element_by_link_text("home page").click()
