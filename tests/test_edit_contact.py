1baf17c62020440dc77859f7f9ceeeea82baba74
ELagunova <elvlagunova@ya.ru>
2021-07-08 23:14:31 +0300

    Добавлена валидация фикстуры перед каждым тестом

--- tests/test_edit_contact.py
+++ tests/test_edit_contact.py
@@ -2,25 +2,17 @@ from model.contact import Contact
 
 
 def test_edit_name(app):
-    app.session.login(username="admin", password="secret")
     app.contact.edit_first(Contact(first_name="Tomas", last_name="Walless"))
-    app.session.logout()
 
 
 def test_edit_contacts_data(app):
-    app.session.login(username="admin", password="secret")
     app.contact.edit_first(Contact(address="702 First Ave, Houston, TX 77027", mobile="+72243212555",
                                    email="tomaswalless77@gmail.com"))
-    app.session.logout()
 
 
 def test_edit_bdate(app):
-    app.session.login(username="admin", password="secret")
     app.contact.edit_first(Contact(bday="12", bmonth="January", byear="1992"))
-    app.session.logout()
 
 
 def test_edit_notes(app):
-    app.session.login(username="admin", password="secret")
     app.contact.edit_first(Contact(notes="Tom, Houston edit"))
-    app.session.logout()
--- tests/test_edit_group.py
+++ tests/test_edit_group.py
@@ -2,18 +2,15 @@ from model.group import Group
 
 
 def test_edit_group_name(app):
-    app.session.login(username="admin", password="secret")
     app.group.edit_first(Group(name="Edit group name"))
     app.session.logout()
 
 
 def test_edit_group_header(app):
-    app.session.login(username="admin", password="secret")
     app.group.edit_first(Group(header="Edit header name"))
     app.session.logout()
 
 
 def test_edit_group_footer(app):
-    app.session.login(username="admin", password="secret")
     app.group.edit_first(Group(footer="Edit footer name"))
     app.session.logout()
